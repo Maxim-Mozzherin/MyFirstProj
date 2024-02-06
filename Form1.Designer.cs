6b4a280b368952a017686372e6c3700a9c6cd98a
Maxim A. Mozzherin <39161853+Maxim-Mozzherin@users.noreply.github.com>
2024-02-06 14:15:44 +0500

    Default
    StartForm

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            label1 = new Label();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.ImeMode = ImeMode.KatakanaHalf;
+            dateTimePicker1.Location = new Point(464, 128);
+            dateTimePicker1.MaxDate = new DateTime(2100, 12, 31, 0, 0, 0, 0);
+            dateTimePicker1.MinDate = new DateTime(2024, 1, 1, 0, 0, 0, 0);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(400, 39);
+            dateTimePicker1.TabIndex = 0;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(870, 128);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(400, 39);
+            dateTimePicker2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(464, 93);
+            label1.Name = "label1";
+            label1.Size = new Size(271, 32);
+            label1.TabIndex = 3;
+            label1.Text = "Начало командировки:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(870, 93);
+            label2.Name = "label2";
+            label2.Size = new Size(258, 32);
+            label2.TabIndex = 4;
+            label2.Text = "Конец командировки:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(66, 125);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(242, 40);
+            comboBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(1288, 128);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 39);
+            button1.TabIndex = 6;
+            button1.Text = "Добавить";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(66, 93);
+            label3.Name = "label3";
+            label3.Size = new Size(137, 32);
+            label3.TabIndex = 7;
+            label3.Text = "Сотрудник:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1450, 866);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(comboBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(dateTimePicker1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private Label label1;
+        private Label label2;
+        private ComboBox comboBox1;
+        private Button button1;
+        private Label label3;
+    }
+}
